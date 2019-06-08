04bd6fbbe4822103a083838083c492e0229b1abe
Josh Choi <joshchoi4881@gmail.com>
2019-06-08 17:10:31 -0400

    Added JavaScript functionality when buttons are pressed: makes buttons disappear and results appear

--- homepage.php
+++ homepage.php
@@ -35,7 +35,7 @@ if(isset($_POST["no"])) {
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	    <style>
-			#button {
+			.button {
 				display: block;
 				margin-left: auto;
 				margin-right: auto;
@@ -64,7 +64,7 @@ if(isset($_POST["no"])) {
 	        	</center>
 	        	<br />
 	            <center>
-	            	<input id="button" type="submit" name="yes" value="Yes">
+	            	<input class="button" onclick="showResult()" type="submit" name="yes" value="Yes">
 	            	<p class="result">
 	            		<?php
 	            			if(database::query("SELECT yes FROM posts WHERE id=:id", array(":id"=>1))) {
@@ -76,7 +76,7 @@ if(isset($_POST["no"])) {
 	            		?>
 	        		</p>
 	    	        <br />
-	    	        <input id="button" type="submit" name="no" value="No">
+	    	        <input class="button" onclick="showResult()" type="submit" name="no" value="No">
 	    	        <p class="result">
 	    	        	<?php
 	    	        		if(database::query("SELECT no FROM posts WHERE id=:id", array(":id"=>1))) {
@@ -91,7 +91,12 @@ if(isset($_POST["no"])) {
 	        </form>
 	    </div>
 	    <script>
-	    	document.getElementById('result1').style.display='block';
+	    	function showResult() {
+	    		document.getElementsByClassName("button")[0].style.display = "none";
+	    		document.getElementsByClassName("button")[1].style.display = "none";
+	    		document.getElementsByClassName("result")[0].style.display = "block";
+	    		document.getElementsByClassName("result")[1].style.display = "block";
+	    	}
 	    </script>
 	</body>
 </html>
