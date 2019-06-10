8cfc5f5e6888ed4d77f9b95c93c14711d826cf6e
Josh Choi <joshchoi4881@gmail.com>
2019-06-10 17:53:48 -0400

    Minor changes to database.php

--- database.php
+++ database.php
@@ -1,17 +1,17 @@
 <?php
-class database {
-	private static function connect() {
-		$pdo = new PDO("mysql:host=ls-d70ca738f55284c3d0b5e0528f91203390e1787c.cvhxfjdexada.us-east-1.rds.amazonaws.com;dbname=dbmaster;charset=utf8", "dbmasteruser", "slantcompany");
-		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		return $pdo;
-	}
-	public static function query($query, $params = array()) {
-		$statement = self::connect()->prepare($query);
-		$statement->execute($params);
-		if (explode(" ", $query)[0] == "SELECT") {
-			$data = $statement->fetchAll();
-			return $data;
+	class database {
+		private static function connect() {
+			$pdo = new PDO("mysql:host=127.0.0.1; dbname=slant; charset=utf8", "root", "");
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			return $pdo;
+		}
+		public static function query($query, $params = array()) {
+			$statement = self::connect()->prepare($query);
+			$statement->execute($params);
+			if (explode(" ", $query)[0] == "SELECT") {
+				$data = $statement->fetchAll();
+				return $data;
+			}
 		}
 	}
-}
 ?>
