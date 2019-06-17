aa7a575252b9caa3bc4785287b7e4a667872911a
Josh Choi <jc4881@columbia.edu>
2019-06-17 16:09:01 -0400

    Finished AJAX functionality

--- result.php
+++ result.php
@@ -1,3 +1,17 @@
 <?php
-	echo "<h1>Hello</h1>";
+	include("database.php");
+	$id = $_GET["id"];
+	$response = $_GET["response"];
+	$yes = database::query("SELECT yes FROM posts WHERE id=:id", array(":id"=>$id))[0]["yes"];
+	$no = database::query("SELECT no FROM posts WHERE id=:id", array(":id"=>$id))[0]["no"];
+	if($response == "yes") {
+		$yes += 1;
+		database::query("UPDATE posts SET yes=:yes WHERE id=:id", array(":yes"=>$yes, ":id"=>$id));
+	} else {
+		$no += 1;
+		database::query("UPDATE posts SET no=:no WHERE id=:id", array(":no"=>$no, ":id"=>$id));
+	}
+	$yesPercent = number_format((float)$yes / ($yes + $no) * 100, 2, ".", "");
+	$noPercent = number_format((float)$no / ($yes + $no) * 100, 2, ".", "");
+	echo "<h1>Yes: ".$yesPercent."%<br />No: ".$noPercent."%</h1>";
 ?>
