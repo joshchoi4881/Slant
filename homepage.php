42932ef6189086f1b65bc4777d9a89ed3d6b98f7
Josh Choi <jc4881@columbia.edu>
2019-06-18 10:43:32 -0400

    Added photos, banner, feedback link, source links, and design

--- homepage.php
+++ homepage.php
@@ -12,6 +12,29 @@
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	    <style>
+	    	.header {
+	    		border-bottom: 2px solid gray;
+	    		padding: 10px 16px;
+	    		background-color: #fff;
+	    	}
+	    	.content {
+				padding: 16px;
+			}
+	    	.sticky {
+  				position: fixed;
+  				top: 0;
+  				width: 100%
+			}
+			.sticky + .content {
+				padding-top: 102px;
+			}
+	    	.logo {
+	    		display: block;
+				margin-left: auto;
+				margin-right: auto;
+	    		width: 20%;
+	    		height: 20%;
+	    	}
 	    	.post {
 	    		text-align: center;
 	    		margin-top: 50px;
@@ -29,6 +52,24 @@
 	    	.post:hover {
 				box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
 			}
+			h3 {
+	    		font-family: arial;
+	    		font-weight: bold;
+	    		font-style: italic;
+	    	}
+	    	#abortion {
+	    		border: 10px solid #2bff00;
+	    		width: 80%;
+	    		height: 80%;
+	    	}
+	    	#bidenTrump {
+				border: 10px solid #f22;
+	    	}
+	    	#impeachment {
+	    		border: 10px solid #2bff00;
+				width: 80%;
+	    		height: 80%;
+	    	}
 			.button {
 				display: inline;
 				width: 30%;
@@ -36,46 +77,73 @@
 		</style>
 	</head>
 	<body>
-		<center>
-			<h1 class="text-primary">Slant</h1>
-		</center>
-	    <div class="post">
-	        <h3>Topic</h3>
-	        <p>Explanation</p>
-	        <br />
-	        <p>Question</p>
-	        <br />
-	    	<input class="button" type="button" name="yes" value="Yes" onclick="showResult(1, this.name)">
-	    	<input class="button" type="button" name="no" value="No" onclick="showResult(1, this.name)">
-	    	<p id="result1"></p>
-	    	<br />
-	        <p>Question</p>
-	        <br />
-	    	<input class="button" type="button" name="yes" value="Yes" onclick="showResult(2, this.name)">
-	    	<input class="button" type="button" name="no" value="No" onclick="showResult(2, this.name)">
-	    	<p id="result2"></p>
-	    </div>
-	    <div class="post">
-	        <h3>Topic</h3>
-	        <p>Explanation</p>
-	        <br />
-	        <p>Question</p>
-	        <br />
-	    	<input class="button" type="button" name="yes" value="Yes" onclick="showResult(3, this.name)">
-	    	<input class="button" type="button" name="no" value="No" onclick="showResult(3, this.name)">
-	    	<p id="result3"></p>
-	    </div>
-	    <div class="post">
-	        <h3>Topic</h3>
-	        <p>Explanation</p>
-	        <br />
-	        <p>Question</p>
-	        <br />
-	    	<input class="button" type="button" name="yes" value="Yes" onclick="showResult(4, this.name)">
-	    	<input class="button" type="button" name="no" value="No" onclick="showResult(4, this.name)">
-	    	<p id="result4"></p>
-	    </div>
+		<div id="myHeader" class="header">
+			<img class="logo" src="photos/Slant.jpg" />
+			<a id="feedback" href="https://www.surveymonkey.com/r/SWFZV2X" target="_blank">Feedback<a>
+		</div>
+		<div class="content">
+		    <div class="post">
+		        <h3>ABORTION</h3>
+		        <img class="accent" src="photos/Accent.png" />
+		        <p><blockquote>“Yes, 6 out of 10 Americans support legal abortion in the first trimester of pregnancy, but nearly the same amount support the Hyde Amendment. Contrary to the loudest voices in the primary and the media, even most Democrats do not view abortion as a positive good. Just 44% of Democrats oppose the Hyde Amendment, and fewer than 1 in 5 support legalizing third trimester abortion, which multiple presidential candidates have endorsed… Biden may be blasted for inconsistency, and he'll certainly come under fire from the far left, but he may be more in line with the average American on abortion than any other candidate in the race.”</blockquote><a href="https://www.washingtonexaminer.com/opinion/joe-bidens-position-on-abortion-is-upsetting-the-left-but-hes-a-lot-closer-to-americas-opinion-than-they-are" target="_blank"> - Tiana Lowe, Washington Examiner</a></p>
+		        <br />
+		        <img id="abortion" src="photos/Abortion.jpg" />
+		        <br />
+		        <br />
+		        <p>Do you support legal abortion in the first trimester of pregnancy?</p>
+		        <br />
+		    	<input class="button" type="button" name="yes" value="Yes" onclick="showResult(1, this.name)">
+		    	<input class="button" type="button" name="no" value="No" onclick="showResult(1, this.name)">
+		    	<p id="result1"></p>
+		    	<br />
+		        <p>Do you support the Hyde Amendment?</p>
+		        <br />
+		    	<input class="button" type="button" name="yes" value="Yes" onclick="showResult(2, this.name)">
+		    	<input class="button" type="button" name="no" value="No" onclick="showResult(2, this.name)">
+		    	<p id="result2"></p>
+		    </div>
+		    <div class="post">
+		        <h3>BIDEN VERSUS TRUMP</h3>
+		        <img class="accent" src="photos/Accent.png" />
+		        <p><blockquote>“Especially bleak is the fact that Trump’s approval rating is more than a dozen points underwater in Wisconsin, Michigan, and Iowa — all states he won in 2016. Of course, a lot can change between now and November of next year. And in fairness to Trump, it is true that state-level polls underestimated his support in 2016…  It’s undeniable, however, that the 2020 polling, taken in its totality, doesn’t look good for Trump right now.”</blockquote><a href="https://www.vox.com/2019/6/11/18661072/trump-campaign-internal-polling-denial" target="_blank"> - Aaron Rupar, Vox</a></p>
+		        <br />
+		        <img id="bidenTrump" src="photos/Biden vs Trump.jpg" />
+		        <br />
+		        <br />
+		        <p>The early polls seem to indicate that the American people prefer Biden over Trump. Do you agree?</p>
+		        <br />
+		    	<input class="button" type="button" name="yes" value="Yes" onclick="showResult(3, this.name)">
+		    	<input class="button" type="button" name="no" value="No" onclick="showResult(3, this.name)">
+		    	<p id="result3"></p>
+		    </div>
+		    <div class="post">
+		        <h3>IMPEACHMENT</h3>
+		        <img class="accent" src="photos/Accent.png" />
+		        <p><blockquote>“The longer Democrats drag this out, the less serious they look. They keep saying they need to consider impeachment without doing it. That makes them look like they’re interested in the politics of it, not the reality of it. The American public is largely ready to move on and voters think this issue can be dealt with at the ballot box… So either the Democrats must rush to do impeachment now or they must abandon it to avoid looking political and getting thrown off message… Bob Mueller may have done the President no favors, but he did not do the Democrats any favors either.”</blockquote><a href="https://theresurgent.com/2019/05/30/bob-mueller-boxes-the-democrats-in-on-impeachment/" target="_blank"> - Erick Erickson, The Resurgent</a></p>
+		        <br />
+		        <img id="impeachment" src="photos/Impeachment.jpeg" />
+		        <br />
+		        <br />
+		        <p>Is the American public largely ready to move on?</p>
+		        <br />
+		    	<input class="button" type="button" name="yes" value="Yes" onclick="showResult(4, this.name)">
+		    	<input class="button" type="button" name="no" value="No" onclick="showResult(4, this.name)">
+		    	<p id="result4"></p>
+		    </div>
+		</div>
 	    <script>
+			window.onscroll = function() {
+				myFunction()
+			};
+			var header = document.getElementById("myHeader");
+			var sticky = header.offsetTop;
+			function myFunction() {
+				if (window.pageYOffset > sticky) {
+    				header.classList.add("sticky");
+  				} else {
+    				header.classList.remove("sticky");
+  				}
+			}
 	    	function showResult(id, response) {
 	    		var xhttp;
 				xhttp = new XMLHttpRequest();
