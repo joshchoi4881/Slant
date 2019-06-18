1d431bb5f7b8d362bcdab74088651da65feeb49e
Josh Choi <jc4881@columbia.edu>
2019-06-18 14:32:09 -0400

    Added metadata

--- homepage.php
+++ homepage.php
@@ -1,7 +1,4 @@
 <!DOCTYPE html>
-<?php
-	include("database.php");
-?>
 <html lang="en">
 	<head>
 		<!-- Global site tag (gtag.js) - Google Analytics -->
@@ -14,6 +11,9 @@
 		</script>
 	    <meta charset="utf-8">
 	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	    <meta name="description" content="Public Opinion Back in the Hands of the People">
+		<meta name="keywords" content="Slant, public opinion, politics, polling">
+		<meta name="author" content="Josh Choi">
 	    <title>Slant</title>
 	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
