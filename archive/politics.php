daebad5257122b430277d1b20ba17ec347345f79
Josh Choi <jc4881@columbia.edu>
2019-06-24 16:25:33 -0400

    Added new photos and a new archive of old posts

---
+++ archive/politics.php
@@ -0,0 +1,75 @@
+<section class="post">
+	<h3>ABORTION</h3>
+	<img class="accent" src="photos/design/accent.png" alt="Slant Accent"/>
+	<blockquote>
+		“Yes, 6 out of 10 Americans support legal abortion in the first trimester of pregnancy, but nearly the same amount support the Hyde Amendment. Contrary to the loudest voices in the primary and the media, even most Democrats do not view abortion as a positive good. Just 44% of Democrats oppose the 
+		<a href="https://en.wikipedia.org/wiki/Hyde_Amendment" target="_blank">
+			Hyde Amendment
+		</a>
+		, and fewer than 1 in 5 support legalizing third trimester abortion, which multiple presidential candidates have endorsed… Biden may be blasted for inconsistency, and he\'ll certainly come under fire from the far left, but he may be more in line with the average American on abortion than any other candidate in the race.”
+	</blockquote>
+	<a href="https://www.washingtonexaminer.com/opinion/joe-bidens-position-on-abortion-is-upsetting-the-left-but-hes-a-lot-closer-to-americas-opinion-than-they-are" target="_blank">
+		 - Tiana Lowe, Washington Examiner
+	</a>
+	<br />
+	<img class="images" src="photos/politics/abortion.jpg" alt="Abortion"/>
+	<br />
+	<br />
+	<p>Do you support legal abortion in the first trimester of pregnancy?</p>
+	<br />
+	<div id="result1">
+		<input class="btn btn-success" type="button" name="yes" value="Yes" onclick="showResult(1, this.name, \'yesno\')">
+		<input class="btn btn-warning" type="button" name="idk" value="Not Sure" onclick="showResult(1, this.name, \'yesno\')">
+		<input class="btn btn-danger" type="button" name="no" value="No" onclick="showResult(1, this.name, \'yesno\')">
+	</div>
+	<br />
+	<p>Do you support the <a href="https://en.wikipedia.org/wiki/Hyde_Amendment" target="_blank">Hyde Amendment</a>?</p>
+	<br />
+	<div id="result2">
+		<input class="btn btn-success" type="button" name="yes" value="Yes" onclick="showResult(2, this.name, \'yesno\')">
+		<input class="btn btn-warning" type="button" name="idk" value="Not Sure" onclick="showResult(2, this.name, \'yesno\')">
+		<input class="btn btn-danger" type="button" name="no" value="No" onclick="showResult(2, this.name, \'yesno\')">
+	</div>
+</section>
+<section class="post">
+    <h3>BIDEN VERSUS TRUMP</h3>
+    <img class="accent" src="photos/design/accent.png" alt="Slant Accent"/>
+    <blockquote>
+    	“Especially bleak is the fact that Trump’s approval rating is more than a dozen points underwater in Wisconsin, Michigan, and Iowa — all states he won in 2016. Of course, a lot can change between now and November of next year. And in fairness to Trump, it is true that state-level polls underestimated his support in 2016…  It’s undeniable, however, that the 2020 polling, taken in its totality, doesn’t look good for Trump right now.”
+    	</blockquote>
+    	<a href="https://www.vox.com/2019/6/11/18661072/trump-campaign-internal-polling-denial" target="_blank">
+    		 - Aaron Rupar, Vox
+    	</a>
+    <br />
+    <img class="images" src="photos/politics/bidenTrump.jpg" alt="Biden Versus Trump"/>
+    <br />
+    <br />
+    <p>The early polls seem to indicate that the American people prefer Biden over Trump. Do you agree?</p>
+    <br />
+    <div id="result3">
+		<input class="btn btn-success" type="button" name="yes" value="Yes" onclick="showResult(3, this.name, \'yesno\')">
+		<input class="btn btn-warning" type="button" name="idk" value="Not Sure" onclick="showResult(3, this.name, \'yesno\')">
+		<input class="btn btn-danger" type="button" name="no" value="No" onclick="showResult(3, this.name, \'yesno\')">
+	</div>
+</section>
+<section class="post">
+    <h3>IMPEACHMENT</h3>
+    <img class="accent" src="photos/design/accent.png" alt="Slant Accent"/>
+    <blockquote>
+    	“The longer Democrats drag this out, the less serious they look. They keep saying they need to consider impeachment without doing it. That makes them look like they’re interested in the politics of it, not the reality of it. The American public is largely ready to move on and voters think this issue can be dealt with at the ballot box… So either the Democrats must rush to do impeachment now or they must abandon it to avoid looking political and getting thrown off message… Bob Mueller may have done the President no favors, but he did not do the Democrats any favors either.”
+    </blockquote>
+    <a href="https://theresurgent.com/2019/05/30/bob-mueller-boxes-the-democrats-in-on-impeachment/" target="_blank">
+    	 - Erick Erickson, The Resurgent
+    </a>
+    <br />
+    <img class="images" src="photos/politics/impeachment.jpeg" alt="Trump Impeachment"/>
+    <br />
+    <br />
+    <p>Is the American public largely ready to move on?</p>
+    <br />
+    <div id="result4">
+		<input class="btn btn-success" type="button" name="yes" value="Yes" onclick="showResult(4, this.name, \'yesno\')">
+		<input class="btn btn-warning" type="button" name="idk" value="Not Sure" onclick="showResult(4, this.name, \'yesno\')">
+		<input class="btn btn-danger" type="button" name="no" value="No" onclick="showResult(4, this.name, \'yesno\')">
+	</div>
+</section>
