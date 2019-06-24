8cfc8d4270754475b48541c39ce66757fd866990
Josh Choi <jc4881@columbia.edu>
2019-06-24 15:15:55 -0400

    Fixed slider, finished sports and film sections

--- result.php
+++ result.php
@@ -43,4 +43,68 @@
 		echo "<p>Average: ".$average."<br /><progress min='0' max='10' value=".$average."></progress>";
 		echo "<p>Total: ".$numTotal."</p><br>";
 	}
+	else if($type == "rate") {
+		$fire = database::query("SELECT fire FROM posts WHERE id=:id", array(":id"=>$id))[0]["fire"];
+		$decent = database::query("SELECT decent FROM posts WHERE id=:id", array(":id"=>$id))[0]["decent"];
+		$trash = database::query("SELECT trash FROM posts WHERE id=:id", array(":id"=>$id))[0]["trash"];
+		if($response == "fire") {
+			$fire += 1;
+			database::query("UPDATE posts SET fire=:fire WHERE id=:id", array(":fire"=>$fire, ":id"=>$id));
+		}
+		else if($response == "decent") {
+			$decent += 1;
+			database::query("UPDATE posts SET decent=:decent WHERE id=:id", array(":decent"=>$decent, ":id"=>$id));
+		}
+		else if($response == "trash") {
+			$trash += 1;
+			database::query("UPDATE posts SET trash=:trash WHERE id=:id", array(":trash"=>$trash, ":id"=>$id));
+		}
+		$total = $fire + $decent + $trash;
+		$firePercent = number_format((float)$fire / $total, 2, ".", "") * 100;
+		$decentPercent = number_format((float)$decent / $total, 2, ".", "") * 100;
+		$trashPercent = number_format((float)$trash / $total, 2, ".", "") * 100;
+		echo "<p>Fire: ".$fire." (".$firePercent."%)<br /><meter min='0' max='100' value=".$firePercent."></meter>
+			<br />Decent: ".$decent." (".$decentPercent."%)<br /><meter min='0' max='100' value=".$decentPercent." low='.00001' high='100' optimum='0'></meter>
+			<br />Trash: ".$trash." (".$trashPercent."%)<br /><meter min='0' max='100' value=".$trashPercent." low='.00001' high='.0001' optimum='0'></meter></p>";
+		echo "<p>Total: ".$total."</p><br>";
+	}
+	else if($type == "react") {
+		$happy = database::query("SELECT happy FROM posts WHERE id=:id", array(":id"=>$id))[0]["happy"];
+		$good = database::query("SELECT good FROM posts WHERE id=:id", array(":id"=>$id))[0]["good"];
+		$neutral = database::query("SELECT neutral FROM posts WHERE id=:id", array(":id"=>$id))[0]["neutral"];
+		$sad = database::query("SELECT sad FROM posts WHERE id=:id", array(":id"=>$id))[0]["sad"];
+		$angry = database::query("SELECT angry FROM posts WHERE id=:id", array(":id"=>$id))[0]["angry"];
+		if($response == "happy") {
+			$happy += 1;
+			database::query("UPDATE posts SET happy=:happy WHERE id=:id", array(":happy"=>$happy, ":id"=>$id));
+		}
+		else if($response == "good") {
+			$good += 1;
+			database::query("UPDATE posts SET good=:good WHERE id=:id", array(":good"=>$good, ":id"=>$id));
+		}
+		else if($response == "neutral") {
+			$neutral += 1;
+			database::query("UPDATE posts SET neutral=:neutral WHERE id=:id", array(":neutral"=>$neutral, ":id"=>$id));
+		}
+		else if($response == "sad") {
+			$sad += 1;
+			database::query("UPDATE posts SET sad=:sad WHERE id=:id", array(":sad"=>$sad, ":id"=>$id));
+		}
+		else if($response == "angry") {
+			$angry += 1;
+			database::query("UPDATE posts SET angry=:angry WHERE id=:id", array(":angry"=>$angry, ":id"=>$id));
+		}
+		$total = $happy + $good + $neutral + $sad + $angry;
+		$happyPercent = number_format((float)$happy / $total, 2, ".", "") * 100;
+		$goodPercent = number_format((float)$good / $total, 2, ".", "") * 100;
+		$neutralPercent = number_format((float)$neutral / $total, 2, ".", "") * 100;
+		$sadPercent = number_format((float)$sad / $total, 2, ".", "") * 100;
+		$angryPercent = number_format((float)$angry / $total, 2, ".", "") * 100;
+		echo "<p>Happy: ".$happy." (".$happyPercent."%)<br /><meter min='0' max='100' value=".$happyPercent."></meter>
+			<br />Good: ".$good." (".$goodPercent."%)<br /><meter min='0' max='100' value=".$goodPercent."></meter>
+			<br />Neutral: ".$neutral." (".$neutralPercent."%)<br /><meter min='0' max='100' value=".$neutralPercent."></meter>
+			<br />Sad: ".$sad." (".$sadPercent."%)<br /><meter min='0' max='100' value=".$sadPercent."></meter>
+			<br />Angry: ".$angry." (".$angryPercent."%)<br /><meter min='0' max='100' value=".$angryPercent."></meter></p>";
+		echo "<p>Total: ".$total."</p><br>";
+	}
 ?>
