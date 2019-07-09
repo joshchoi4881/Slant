a0c35cb503cbc87ec5092e3b28651a24669114c5
Josh Choi <joshchoi4881@gmail.com>
2019-07-09 16:40:53 -0400

    Added sign-up and login functionality

---
+++ login.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<?php
+	include("database.php");
+	if (isset($_POST["login"])) {
+		$username = $_POST["username"];
+		$password = $_POST["password"];
+		if (database::query("SELECT username FROM users WHERE username=:username", array(":username"=>$username))) {
+			if (password_verify($password, database::query("SELECT password FROM users WHERE username=:username", array(":username"=>$username))[0]["password"])) {
+				$cstrong = True;
+				$token = bin2hex(openssl_random_pseudo_bytes(64, $cstrong));
+				$userId = database::query("SELECT id FROM users WHERE username=:username", array(":username"=>$username))[0]["id"];
+				database::query("INSERT INTO loginTokens VALUES (:id, :token, :userId)", array(":id"=>null, ":token"=>sha1($token), ":userId"=>$userId));
+				setcookie("SLANT_ID", $token, time() + 60 * 60 * 24 * 7, "/", NULL, NULL, TRUE);
+				setcookie("SLANT_ID_", "1", time() + 60 * 60 * 24 * 3, "/", NULL, NULL, TRUE);
+				header("Location: homepage.php?username=$username");
+			} else {
+				echo "Incorrect password";
+			}
+		} else {
+			echo "User does not exist";
+		}
+	}
+?>
+<html lang="en">
+	<head>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-138974831-1"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+  			function gtag(){dataLayer.push(arguments);}
+  			gtag('js', new Date());
+			gtag('config', 'UA-138974831-1');
+		</script>
+		<!--	-->
+	    <meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	    <meta name="description" content="The Marketplace for Public Opinion">
+		<meta name="keywords" content="Slant, public opinion, polling">
+		<meta name="author" content="Josh Choi">
+	    <title>Slant</title>
+	    <link rel="stylesheet" type="text/css" href="css/slant.css">
+	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<style>
+			.login {
+				text-align: center;
+			}
+			form {
+				display: inline-block;
+			}
+		</style>
+	</head>
+	<body>
+		<header id="myHeader" class="header">
+			<img class="logo" src="photos/design/slant.jpg" alt="Slant Logo"/>
+			<nav>
+				<div>
+					<a href="homepage.php">Home</a>
+					<a href="signUp.php">Sign Up</a>
+				</div>
+			</nav>
+		</header>
+		<div class="login">
+	        <form action="login.php" method="POST">
+	        	<h1>Slant</h1>
+	            <h2>Login Form</h2>
+	            <input type="text" name="username" value="" placeholder="Username" required autofocus>
+	            <br />
+	            <br />
+	            <input type="password" name="password" value="" placeholder="Password" required>
+	            <br />
+	            <br />
+	            <button class="btn btn-primary" type="submit" name="login">Login</button>
+	            <br />
+	            <br />
+	            <a href="forgotPassword.php">Forgot password?</a>
+	        	<br />
+	        	<br />
+	        	<a href="signUp.php">Sign Up</a>
+	        </form>
+		</div>
+		<script src="js/slant.js">
+		</script>
+	</body>
+</html>
