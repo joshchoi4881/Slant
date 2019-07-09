5fead450c5a6a14bde90fe1424511a3725766b88
Josh Choi <jc4881@columbia.edu>
2019-07-09 17:16:41 -0400

    Added login function class and edited homepage to look different for people logged in with accounts

--- signUp.php
+++ signUp.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <?php
-	include("database.php");
+	include("classes/database.php");
 	if (isset($_POST["createAccount"])) {
 		$firstName = $_POST["firstName"];
 		$lastName = $_POST["lastName"];
