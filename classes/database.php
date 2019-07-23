708483d1cafd6c90127beef8aacd2eba147ff30c
Josh Choi <joshchoi4881@gmail.com>
2019-07-22 20:43:38 -0400

    Deleted photos which will now be uploaded through database; removed the requirement to input a quote on the content page

--- classes/database.php
+++ classes/database.php
@@ -1,12 +1,12 @@
 <?php
-	class Database {
+	class database {
 		private static function connect() {
-			$pdo = new PDO("mysql:host=127.0.0.1; dbname=slant; charset=utf8", "root", "");
+			$pdo = new PDO("mysql:host=ls-d70ca738f55284c3d0b5e0528f91203390e1787c.cvhxfjdexada.us-east-1.rds.amazonaws.com; dbname=slant; charset=utf8", "dbmasteruser", "slantcompany");
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			return $pdo;
 		}
 		public static function query($query, $params = array()) {
-			$pdo = self::connect();
+			$pdo = self()::connect();
 			$statement = $pdo->prepare($query);
 			$statement->execute($params);
 			if (explode(" ", $query)[0] == "SELECT") {
